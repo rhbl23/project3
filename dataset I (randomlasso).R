0f8fde1775dd6b92b01a3d8f937f624e52d70692
Shuyi Peng <148757625+rhbl23@users.noreply.github.com>
2025-01-07 08:58:35 +0000

    Update dataset I (randomlasso).R

--- dataset I (randomlasso).R
+++ dataset I (randomlasso).R
@@ -142,6 +142,8 @@ compute_aggregated_metrics <- function(res_lasso) {
   ))
 }
 
+boxplot(res_random[, "mse"] ~ res_random[, "nn"],main='randomlasso',col="lightgreen")
+
 compute_aggregated_metrics(res_random)
 $sensitivity
 [1] 0.8866667
